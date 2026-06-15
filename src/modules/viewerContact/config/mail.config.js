9e6ca626010b23a932da1d9789fd954f67e5d94f
Sujil <sujilsmathew54@gmail.com>
2026-06-15 11:10:01 +0530

    updated gmail to smtp used in nodemailer to send mail, also added try-catch to the mail service

--- src/modules/viewerContact/config/mail.config.js
+++ src/modules/viewerContact/config/mail.config.js
@@ -2,7 +2,9 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.OWNER_EMAIL_USER,
         pass: process.env.OWNER_EMAIL_PASS,
@@ -11,8 +13,7 @@ const transporter = nodemailer.createTransport({
 
 transporter.verify((err, success) => {
     if (err) {
-        return new Error('Mail Error');
-        console.error("Mail transporter not running");
+        console.error("Mail transporter not running", err);
     } else {
         console.log("Mail transporter running");
     };
--- src/modules/viewerContact/service/mail.services.js
+++ src/modules/viewerContact/service/mail.services.js
@@ -2,26 +2,33 @@ const transporter = require('../config/mail.config');
 require('dotenv').config();
 
 const sendContactMail = async data => {
-    await transporter.sendMail({
-        from: process.env.OWNER_EMAIL_USER,
-        to: data.mail,
+    try {
+        console.log("Attempting mail send");
 
-        subject: `Portfolio Contact: ${data.subject}`,
+        await transporter.sendMail({
+            from: process.env.OWNER_EMAIL_USER,
+            to: data.mail,
+            replyTo: data.mail,
+            subject: `Portfolio Contact: ${data.subject}`,
 
-        html: `
-            <h2>New Contact Form</h2>
+            html: `
+                <h2>New Contact Form</h2>
 
-            <p><b>Name: </b>${data.name}</p>
+                <p><b>Name: </b>${data.name}</p>
 
-            <p><b>Email: </b>${data.mail}</p>
-            
-            <p><b>Viewer Type: </b>${data.viewer}</p>
+                <p><b>Email: </b>${data.mail}</p>
+                
+                <p><b>Viewer Type: </b>${data.viewer}</p>
 
-            <p><b>Message:</b></p>
+                <p><b>Message:</b></p>
 
-            <p>${data.message}</p>
-        `
-    });
+                <p>${data.message}</p>
+            `
+        });
+    } catch (err) {
+        console.error("Mail send failed", err);
+        throw err;
+    }
 };
 
 module.exports = {
